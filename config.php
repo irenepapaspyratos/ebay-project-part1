0af03867bac14a70c2336473ed306aea750f3f02
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2023-07-30 23:20:47 +0200

    Minor updates

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 return [
     'ebay' => [
         'xml_api_production' => 'https://api.ebay.com/ws/api.dll',
-        'combatibility_level' => 1311,    // 2023-May-19,
+        'combatibility_level' => 1311,    // API version 2023-May-19,
         'site_id' => 77,  // USA: 0, Germany: 77
     ],
 ];
