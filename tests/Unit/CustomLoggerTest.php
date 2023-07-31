fd09da482f10a613bb506f189712190476d0bb68
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2023-07-31 03:41:18 +0200

    Update CustomLoggerTest with comments and shorter code

--- tests/Unit/CustomLoggerTest.php
+++ tests/Unit/CustomLoggerTest.php
@@ -5,32 +5,43 @@ namespace Tests\Unit;
 use App\Utility\CustomLogger;
 use Codeception\Test\Unit;
 
+/**
+ * The CustomLoggerTest class is a unit test class that tests the functionality of the CustomLogger class 
+ * in writing error and info messages to the correct log files. 
+ */
 class CustomLoggerTest extends Unit {
 
     protected $tester;
+    private $customLogger;
     private $errorLogPath;
     private $infoLogPath;
-    private $customLogger;
+    private $nonWritableLogPath;
 
+    /**
+     * The function initializes the paths for log files 
+     * and creates a new instance of the CustomLogger class to be tested.
+     */
     protected function _before() {
 
-        // Define the paths for the log files
+        // Clean up all test files (just in case...)
+        $this->tester->cleanDir(codecept_data_dir());
+
+        // Define the temporary paths for writable test log files
         $this->errorLogPath = codecept_data_dir('error.log');
         $this->infoLogPath = codecept_data_dir('info.log');
 
-        // Create a new CustomLogger instance
-        $this->customLogger = new CustomLogger($this->errorLogPath, $this->infoLogPath);
+        // Define the temporary path for a non-writable test log file
+        $this->nonWritableLogPath = codecept_data_dir('non_writable.log');
+        touch($this->nonWritableLogPath);
+        chmod($this->nonWritableLogPath, 0444);
     }
 
+    /**
+     * The function cleans up all test files by deleting them after each test.
+     */
     protected function _after() {
 
-        // Clean up the log files after each test
-        if (file_exists($this->errorLogPath)) {
-            $this->tester->deleteFile($this->errorLogPath);
-        }
-        if (file_exists($this->infoLogPath)) {
-            $this->tester->deleteFile($this->infoLogPath);
-        }
+        $this->tester->cleanDir(codecept_data_dir());
     }
 
     /**
@@ -38,16 +49,21 @@ class CustomLoggerTest extends Unit {
      */
     public function testErrorLogWritesToCorrectFile() {
 
+        // Create a new CustomLogger instance with test log files
+        $this->customLogger = new CustomLogger($this->errorLogPath, $this->infoLogPath);
+
         // Arrange
         $message = 'Test error message';
 
         // Act
         $this->customLogger->error_log($message);
 
-        // Assert
+        // Assert that the $result is true, meaning that the error message was successfully written to the error log 
         $this->tester->seeFileFound($this->errorLogPath);
-        $this->tester->openFile($this->errorLogPath);
         $this->tester->seeInThisFile($message);
+
+        // Assert that the info file still is not existing
+        $this->assertFalse(file_exists($this->infoLogPath));
     }
 
     /**
@@ -55,15 +71,56 @@ class CustomLoggerTest extends Unit {
      */
     public function testInfoLogWritesToCorrectFile() {
 
+        // Create a new CustomLogger instance with test log files
+        $this->customLogger = new CustomLogger($this->errorLogPath, $this->infoLogPath);
+
         // Arrange
         $message = 'Test info message';
 
         // Act
         $this->customLogger->info_log($message);
 
-        // Assert
+        // Assert that the $result is true, meaning that the error message was successfully written to the info log 
         $this->tester->seeFileFound($this->infoLogPath);
-        $this->tester->openFile($this->infoLogPath);
         $this->tester->seeInThisFile($message);
+
+        // Assert that the error file still is not existing
+        $this->assertFalse(file_exists($this->errorLogPath));
+    }
+
+    /**
+     * The function tests if an error message cannot be written to a file with missing permissions
+     */
+    public function testErrorLogCannotWriteToFile() {
+
+        // Create a new CustomLogger instance with test log files
+        $this->customLogger = new CustomLogger($this->nonWritableLogPath, $this->infoLogPath);
+
+        // Arrange
+        $message = 'Test error message';
+
+        // Assert that an exception of type \Codeception\Exception\Warning is thrown
+        $this->expectException(\Codeception\Exception\Warning::class);
+
+        // Act
+        $this->customLogger->error_log($message);
+    }
+
+    /**
+     * The function tests if an info message cannot be written to a file with missing permissions
+     */
+    public function testInfoLogCannotWriteToFile() {
+
+        // Create a new CustomLogger instance with test log files
+        $this->customLogger = new CustomLogger($this->errorLogPath, $this->nonWritableLogPath);
+
+        // Arrange
+        $message = 'Test info message';
+
+        // Assert that an exception of type \Codeception\Exception\Warning is thrown
+        $this->expectException(\Codeception\Exception\Warning::class);
+
+        // Act
+        $this->customLogger->info_log($message);
     }
 }
