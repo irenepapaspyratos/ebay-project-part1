1a858c28e8ee54636c30355d3ab4de321953342d
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2023-08-25 09:36:40 +0200

    Update main script regarding the loading of the environment variables.

--- scripts/main.php
+++ scripts/main.php
@@ -6,6 +6,10 @@ require __DIR__ . '/../vendor/autoload.php';
 // Load configuration values
 $config = require __DIR__ . '/../config.php';
 
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$errors = $dotenv->load();
+
 use App\Service\EbayApiService;
 use App\Utility\CustomCurl;
 use App\Utility\CustomLogger;
