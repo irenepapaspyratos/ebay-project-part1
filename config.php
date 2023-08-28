dd84f4244441a15d1bed9d4d540c4301e6765b26
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2023-08-28 22:16:17 +0200

    Update eBay Api Version

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 // Set global configurations
 return [
     'ebay' => [
-        'xml_api_production' => 'https://api.ebay.com/ws/api.dll',
-        'combatibility_level' => 1311,    // API version 2023-May-19,
+        'xml_api_production' => 'https://api.ebay.com/ws/api.dll',  // Sandbox: 'https://api.sandbox.ebay.com/ws/api.dll'
+        'combatibility_level' => 1323,  // API version 2023-May-25,
         'site_id' => 77,  // USA: 0, Germany: 77
     ],
 ];
