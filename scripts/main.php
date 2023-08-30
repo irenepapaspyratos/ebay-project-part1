18ee1c80c4350cbe101bae41ea1923507b1138f7
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2023-08-30 22:17:19 +0200

    Add bootstrap file to app and include it in app and test environment

--- scripts/main.php
+++ scripts/main.php
@@ -1,31 +1,26 @@
 <?php
 
-// Load classes
-require __DIR__ . '/../vendor/autoload.php';
+// Include the `bootstrap.php`to set up the application's environment
+require __DIR__ . '/../bootstrap.php';
 
-// Load configuration values
-$config = require __DIR__ . '/../config.php';
-
-// Load environment variables
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$errors = $dotenv->load();
-
-use App\Enum\Ebay\EbayGranularityLevel;
 use App\Service\EbayApiService;
 use App\Utility as Utils;
 
-// Assign environment and configuration variables
+// Assign values
+$initialActiveIdsFile = __DIR__ . '/../data/ebay/initial_active_ids.xml';
 $ebayApiUrl = $config['ebay']['xml_api_production'];
 $ebayApiToken = $_ENV['EBAY_API_TOKEN'];
 $ebayCompatLevel = $config['ebay']['combatibility_level'];
 $ebaySiteId = $config['ebay']['site_id'];
 
-// Create services
-$xmlUtils = new Utils\XmlUtils();
+// Create instance of EbayApiService and necessary classes
 $customLogger = new Utils\CustomLogger();
 $ebayCurl = new Utils\CustomCurl($ebayApiUrl);
 $ebayDateUtils = new Utils\DateUtils();
+$xmlUtils = new Utils\XmlUtils();
 $ebayApiService = new EbayApiService($xmlUtils, $customLogger, $ebayCurl, $ebayDateUtils, $ebayApiToken, $ebayCompatLevel, $ebaySiteId);
 
-if (!glob(__DIR__ . '/../data/ebay/initial_active_ids' . '*.xml'))
+// Create only once the initial file with all ids of a seller's active item listings
+if (!glob(str_replace('.xml', '*.xml', $initialActiveIdsFile)))
     $ebayApiService->storeSellerList();
+print_r($ebayApiService->getItemDetails(234678384042));
