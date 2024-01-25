98b084c42a7fc191dfd0d55c0bd6beefe59a37bc
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2024-01-25 01:15:38 +0100

    Refactor CustomCurl incl. Test

--- tests/Unit/Utility/CustomCurlTest.php
+++ tests/Unit/Utility/CustomCurlTest.php
@@ -75,4 +75,41 @@ class CustomCurlTest extends Unit {
         $this->assertIsString($response);
         $this->assertEquals('Response-String', $response);
     }
+
+    /**
+     * Tests whether the response is a string with expected content by
+     * setting up a mock response for a specific endpoint on a mock web server.
+     */
+    public function testExecuteCurlWithHeadersSuccess() {
+
+        // Setting up a mock response for a specific endpoint on the mock web server with headers.
+        $responseHeaders = [
+            'X-Powered-By' => 'PHP/8.2.8',
+            'Content-type' => 'text/html; charset=UTF-8',
+        ];
+
+        $this->apiUrl = $this->server->setResponseOfPath(
+            '/test-endpoint',
+            new \donatj\MockWebServer\Response('Response-String', $responseHeaders, 200)
+        );
+
+        // Arange
+        $this->customCurl = new CustomCurl($this->apiUrl);
+
+        // Act
+        $response = $this->customCurl->executeCurl([], '', true);
+
+
+        // Assert that the response of the 'executeCurl' method is a string with expected content
+        $this->assertIsArray($response);
+
+        $this->assertIsString($response['body']);
+        $this->assertEquals('Response-String', $response['body']);
+
+        $this->assertIsString($response['headers']);
+        $this->assertStringContainsString('Content-type: text/html; charset=UTF-8', $response['headers']);
+        // As Mock gives changing values, the following are just tested for existence
+        $this->assertStringContainsString('Host:', $response['headers']);
+        $this->assertStringContainsString('Date:', $response['headers']);
+    }
 }
